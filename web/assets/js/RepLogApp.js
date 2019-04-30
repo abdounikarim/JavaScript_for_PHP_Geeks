6ed5c53a21640e670ebd1fe50b2cd9c91ac3a19a
Karim <abdounikarim@gmail.com>
2019-04-30 17:13:03 +0200

    Catching a Failed Promise

--- web/assets/js/RepLogApp.js
+++ web/assets/js/RepLogApp.js
@@ -33,11 +33,10 @@
             var self= this;
             $.ajax({
                 url: Routing.generate('rep_log_list'),
-                success: function (data) {
-                    $.each(data.items, function (key, repLog) {
-                        self._addRow(repLog);
-                    })
-                }
+            }).then(function(data){
+                $.each(data.items, function (key, repLog) {
+                    self._addRow(repLog);
+                })
             });
         },
 
@@ -61,13 +60,12 @@
             var self = this;
             $.ajax({
                 url: deleteUrl,
-                method: 'DELETE',
-                success: function(){
-                    $row.fadeOut('normal', function () {
-                        $(this).remove();
-                        self.updateTotalWeightLifted();
-                    });
-                }
+                method: 'DELETE'
+            }).then(function(data) {
+                $row.fadeOut('normal', function () {
+                    $(this).remove();
+                    self.updateTotalWeightLifted();
+                });
             });
         },
         handleRowClick: function()
@@ -84,31 +82,23 @@
                 formData[fieldData.name] = fieldData.value;
             });
             var self = this;
-            $.ajax({
-                url: $form.data('url'),
-                method: 'POST',
-                data: JSON.stringify(formData),
-                success: function (data) {
-                    // todo
-                    self._clearForm();
-                    self._addRow(data);
-                },
-                error: function(jqXHR)
-                {
-                    var errorData = JSON.parse(jqXHR.responseText);
-                    self._mapErrorsToForm(errorData.errors);
-                }
-            }).then(function (data) {
-                console.log('I am successful');
-                console.log(data);
-
-                return data;
-            }).then(function (data) {
-                console.log('another message');
-                console.log(data);
+            this._saveRepLog(formData)
+            .then(function (data) {
+                self._clearForm();
+                self._addRow(data);
+            }).catch(function(jqXHR){
+                var errorData = JSON.parse(jqXHR.responseText);
+                self._mapErrorsToForm(errorData.errors);
             });
         },
-        
+        _saveRepLog: function(data) {
+            return $.ajax({
+                url: Routing.generate('rep_log_new'),
+                method: 'POST',
+                data: JSON.stringify(data),
+
+            });
+        },
         _mapErrorsToForm: function (errorData) {
 
             var $form = this.$wrapper.find(this._selectors.newRepForm);
